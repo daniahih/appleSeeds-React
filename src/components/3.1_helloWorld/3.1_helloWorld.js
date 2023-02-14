5be6a467116420fc670a4fc745d5adf9d872548c
daniahih <daniahih310@gmail.com>
2023-02-14 13:51:52 +0200

    added 3.1 hello world

---
+++ src/components/3.1_helloWorld/3.1_helloWorld.js
@@ -0,0 +1,6 @@
+function Helloworld(){
+    return <h1>
+        Hello World ! 
+    </h1>
+}
+ export default Helloworld;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Helloworld from './components/3.1_helloWorld/3.1_helloWorld';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App />
+ <Helloworld/>
 
 );
 
